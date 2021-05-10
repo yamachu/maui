2ccb1c5965adedca278dd2390bf028b6d97b077b
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-05-10 19:33:27 +0200

    Fixed MainPage parameters

--- src/Controls/samples/Controls.Sample/Pages/CustomNavigationPage.xaml.cs
+++ src/Controls/samples/Controls.Sample/Pages/CustomNavigationPage.xaml.cs
@@ -1,16 +1,14 @@
-﻿using Microsoft.Maui.Controls;
+﻿using System;
+using Microsoft.Maui.Controls;
+using Maui.Controls.Sample.ViewModels;
 
 namespace Maui.Controls.Sample.Pages
 {
     public partial class CustomNavigationPage : NavigationPage
     {
-        public CustomNavigationPage()
-        {
-            InitializeComponent();
-        }
-
-        public CustomNavigationPage(Page root) : base(root)
-        {
+        public CustomNavigationPage(IServiceProvider services, MainViewModel viewModel) :
+            base(new MainPage(services, viewModel))
+		{
             InitializeComponent();
         }
     }
--- src/Controls/samples/Controls.Sample/Pages/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample/Pages/MainPage.xaml.cs
@@ -1,10 +1,11 @@
-﻿using Maui.Controls.Sample.ViewModels;
+﻿using System;
+using Maui.Controls.Sample.ViewModels;
 
 namespace Maui.Controls.Sample.Pages
 {
 	public partial class MainPage
 	{
-		public MainPage(MainViewModel viewModel)
+		public MainPage(IServiceProvider services, MainViewModel viewModel)
 		{
 			InitializeComponent();
 
